79c41662fd924dd5f159ac4fc2e8f4dcce7fc227
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-05 07:11:13 +0900

    ツイートボタンの設置

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -9,6 +9,7 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
 
 function sidebar() {
   return (
@@ -27,6 +28,9 @@ function sidebar() {
       <SidebarOption text="もっとみる" Icon={MoreHorizIcon} />
 
       {/* ツイートボタン */}
+      <Button variant="outlined" className="sidebar__tweet">
+        ツイートする
+      </Button>
     </div>
   );
 }
