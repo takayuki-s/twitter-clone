5edb9a25c6c3af600d395c379e74714ced46abf1
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-04 09:20:20 +0900

    サイドバー作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function sidebar() {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  );
+}
+
+export default sidebar;
