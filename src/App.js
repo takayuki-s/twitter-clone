2e1fddacecf217bd554d5072e8a3bfaab4396557
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-04 09:28:52 +0900

    ツイッターアイコン表示、ファイル名訂正

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Sidebar from "./components/sidebar/sidebar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function sidebar() {
   return (
     <div className="sidebar">
       {/* ツイッターアイコン */}
+      <TwitterIcon className="sidebar__twitterIcon" />
 
       {/* SidebarOption */}
       {/* SidebarOption */}
