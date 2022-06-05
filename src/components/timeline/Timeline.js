5ae398e1a272a23907a12542fee7c7b5a8e2e5ec
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-06 07:25:42 +0900

    Timelineコンポーネント作成、CSS適用

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Timeline.css";
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline__header">
+        <h2>ホーム</h2>
+      </div>
+
+      {/* TweetBox */}
+
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Timeline;
