9b2ffd18df847a199633f1b40b0222d046955d29
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-10 08:40:45 +0900

    ツイート投稿（入力）

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,21 +1,37 @@
 import { Avatar, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    // firebaseのデータベースに追加する
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar />
-          <input placeholder="いまどうしてる？" type="text"></input>
+          <input
+            placeholder="いまどうしてる？"
+            type="text"
+            onChange={(e) => setTweetMessage(e.target.value)}
+          ></input>
         </div>
         <input
           className="tweetBox__imageInput"
           placeholder="画像のURLを入力してください"
           type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
         ></input>
-        <Button className="tweetBox__tweetButton" type="submit">
+        <Button
+          className="tweetBox__tweetButton"
+          type="submit"
+          onClick={sendTweet}
+        >
           ツイートする
         </Button>
       </form>
