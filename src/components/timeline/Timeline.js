07e0895aa3b67506f859cd558a8841cbaa823e19
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-10 08:34:15 +0900

    マウント時だけデータを取得
    key={post.text}は一時的なものであるので、本来は一意な値を入れておくように

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
@@ -8,10 +8,12 @@ import { collection, getDocs } from "firebase/firestore";
 function Timeline() {
   const [posts, setPosts] = useState([]);
 
-  const postData = collection(db, "posts");
-  getDocs(postData).then((querySnapshot) => {
-    setPosts(querySnapshot.docs.map((doc) => doc.data()));
-  });
+  useEffect(() => {
+    const postData = collection(db, "posts");
+    getDocs(postData).then((querySnapshot) => {
+      setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
   return (
     <div className="timeline">
@@ -26,6 +28,7 @@ function Timeline() {
       {/* Post */}
       {posts.map((post) => (
         <Post
+          key={post.text}
           displayName={post.displayName}
           username={post.username}
           verified={post.verified}
