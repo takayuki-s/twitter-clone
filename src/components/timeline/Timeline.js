192429de9f13e688e0f615c4dc643182b76d2c9b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-10 08:28:44 +0900

    取り出したデータをPost.jsに渡して出力

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
@@ -6,9 +6,11 @@ import db from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 function Timeline() {
+  const [posts, setPosts] = useState([]);
+
   const postData = collection(db, "posts");
   getDocs(postData).then((querySnapshot) => {
-    console.log(querySnapshot.docs.map((doc) => doc.data()));
+    setPosts(querySnapshot.docs.map((doc) => doc.data()));
   });
 
   return (
@@ -22,19 +24,16 @@ function Timeline() {
       <TweetBox />
 
       {/* Post */}
-      <Post
-        displayName="プログラミングチュートリアル"
-        username="Shin_Engineer"
-        verified={true}
-        text="初めてのツイート"
-        avatar="http://shincode.info/wp-content/uploads/2021/12/icon.png"
-        image="https://source.unsplash.com/random"
-      />
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 }
