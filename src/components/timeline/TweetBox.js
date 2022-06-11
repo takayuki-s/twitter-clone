0a18ec07a23199c66c80d2e32460536452d6b58a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-11 09:05:23 +0900

    ツイート投稿（DB保存）

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,5 +1,7 @@
 import { Avatar, Button } from "@mui/material";
 import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../firebase";
 import "./TweetBox.css";
 
 function TweetBox() {
@@ -8,6 +10,17 @@ function TweetBox() {
 
   const sendTweet = (e) => {
     // firebaseのデータベースに追加する
+    e.preventDefault();
+    addDoc(collection(db, "posts"), {
+      displayName: "プログラミングチュートリアル",
+      username: "Shin_Engineer",
+      verified: true,
+      text: tweetMessage,
+      avatar: "http://shincode.info/wp-content/uploads/2021/12/icon.png",
+      image: tweetImage,
+    });
+    setTweetMessage("");
+    setTweetImage("");
   };
 
   return (
@@ -16,12 +29,14 @@ function TweetBox() {
         <div className="tweetBox__input">
           <Avatar />
           <input
+            value={tweetMessage}
             placeholder="いまどうしてる？"
             type="text"
             onChange={(e) => setTweetMessage(e.target.value)}
           ></input>
         </div>
         <input
+          value={tweetImage}
           className="tweetBox__imageInput"
           placeholder="画像のURLを入力してください"
           type="text"
